5b1e0b499a54be96199d31b9e7ff1c98e9e8f67e
Mikayda Mills <mills.ma12@gmail.com>
2019-07-01 14:42:04 -0400

    change index files to containers

---
+++ client/src/components/Compass.jsx
@@ -0,0 +1,83 @@
+// import React, { Component } from "react";
+// import Nav from "./Nav";
+// import { Route, Link, withRouter } from "react-router-dom"; //import Tooltip from "@material-ui/core/Tooltip";
+// import ReactChartkick, { LineChart, PieChart } from "react-chartkick";
+// import Chart from "chart.js";
+// import { connect } from "react-redux";
+// import QueryBar from "./QueryBar";
+// import { createTickerVal } from "../utilities";
+// import { compassHistoryDataFetch } from "../actions";
+// ReactChartkick.addAdapter(Chart);
+
+// class Compass extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   componentDidMount() {
+//     this.props.dispatch(
+//       compassHistoryDataFetch(
+//         createTickerVal(this.props.state.appReducer.ticker)
+//       )
+//     );
+//   }
+
+//   componentDidUpdate(prevProps) {
+//     if (
+//       prevProps.state.appReducer.ticker !== this.props.state.appReducer.ticker
+//     ) {
+//       console.log(
+//         "componentDidUpdate COMPASS",
+//         this.props.state.appReducer.ticker
+//       );
+//       this.props.dispatch(
+//         compassHistoryDataFetch(
+//           createTickerVal(this.props.state.appReducer.ticker)
+//         )
+//       );
+//     }
+//   }
+
+//   render() {
+//     const { chartData } = this.props.state.appReducer;
+//     const lineChart = (
+//       <div>
+//         <LineChart
+//           data={chartData}
+//           title={createTickerVal()}
+//           min={null}
+//           max={null}
+//           width={"800px"}
+//           height={"400px"}
+//           hAxis={"Time"}
+//           vAxis={"Price"}
+//         />
+//       </div>
+//     );
+//     return (
+//       <div className="compass-container">
+//         <Nav />
+//         <div className="wrapper">
+//           <div className="wave" />
+//         </div>
+//         <div className="page-titles">
+//           <h1>the Compass</h1>
+//           <p>
+//             red skies at morn, traders take warn <br /> red skies at night,
+//             traders delight
+//           </p>
+//         </div>
+//         <QueryBar
+//           redirect={() =>
+//             this.props.history.push(
+//               `/compass/${this.props.state.appReducer.ticker}`
+//             )
+//           }
+//         />
+//         {chartData && <div className="chart-container">{lineChart}</div>}
+//       </div>
+//     );
+//   }
+// }
+// const mapStateToProps = state => ({ state });
+// export default withRouter(connect(mapStateToProps)(Compass));
---
+++ client/src/components/Records.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import Nav from "./Nav";
+
+// const Records = props => {
+//   return (
+//     <div className="records-container">
+//       <Nav />
+//       <h1>Welcome to Records</h1>
+//       <h2>Transaction History:</h2>
+//     </div>
+//   );
+// };
+
+// export default Records;
